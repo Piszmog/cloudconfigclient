19a400f30cd26d51e97532d97a818a720a36da31
Piszmog <piszmogcloud@gmail.com>
2018-08-11 13:06:38 -0600

    start adding docs

--- client/client.go
+++ client/client.go
@@ -6,19 +6,23 @@ import (
 	"net/http"
 )
 
+// Client for the Config Server
 type ConfigClient struct {
 	Clients []CloudClient
 }
 
+// Client interacting with the Config Server's REST APIs
 type CloudClient interface {
 	Get(uriVariables ...string) (resp *http.Response, err error)
 }
 
+// Client that wraps http.Client and the base Uri of the http client
 type Client struct {
 	configUri  string
 	httpClient *http.Client
 }
 
+// Get performs a REST GET
 func (client Client) Get(uriVariables ...string) (resp *http.Response, err error) {
 	fullUrl := net.CreateUrl(client.configUri, uriVariables...)
 	response, err := client.httpClient.Get(fullUrl)
