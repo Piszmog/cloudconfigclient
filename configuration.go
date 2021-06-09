68aebc8f7c85f73f5fda090d18a922d2648b9d5a
Piszmog <piszmogcode@gmail.com>
2021-06-09 14:33:39 -0600

    Fix error check

--- configuration.go
+++ configuration.go
@@ -72,7 +72,7 @@ func (c *Client) GetConfiguration(applicationName string, profiles ...string) (S
 	paths := []string{applicationName, joinProfiles(profiles)}
 	for _, client := range c.clients {
 		if err := client.GetResource(paths, nil, &source); err != nil {
-			if errors.As(err, &notFoundError) {
+			if errors.Is(err, notFoundError) {
 				continue
 			}
 			return Source{}, err
